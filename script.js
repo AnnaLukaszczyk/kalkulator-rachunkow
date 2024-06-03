4301161d8ee669ad808f7e77ccbdc7dccde13927
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-06-03 21:36:20 +0200

    the necessary items have been downloaded

---
+++ script.js
@@ -0,0 +1,8 @@
+const price = document.querySelector("#price");
+const people = document.querySelector("#people");
+const tip = document.querySelector("#tip");
+const countButton = document.querySelector(".count");
+const error = document.querySelector(".error");
+const costInfo = document.querySelector(".cost-info");
+const cost = document.querySelector(".cost");
+
