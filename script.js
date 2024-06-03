6b41c6497775a9bfd233b7fe4f8402f7be1ddfd2
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-06-03 21:38:15 +0200

    areAllFilled function created

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const error = document.querySelector(".error");
 const costInfo = document.querySelector(".cost-info");
 const cost = document.querySelector(".cost");
 
+const areAllFilled = () => {
+	if (price.value !== "" && people.value !== "" && tip.value !== "1") {
+		error.style.visibility = "hidden";
+	} else {
+		error.style.visibility = "visible";
+	}
+};
+
+countButton.addEventListener("click", areAllFilled);
